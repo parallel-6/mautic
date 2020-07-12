5cc338c964a313e49bc0a2a09b9567425c5bd165
Mohammad Abu Musa <m.abumusa@usersproof.com>
2020-07-12 11:14:06 +0300

    modified:   app/bundles/EmailBundle/Model/TransportType.php

--- app/bundles/EmailBundle/Model/TransportType.php
+++ app/bundles/EmailBundle/Model/TransportType.php
@@ -17,6 +17,7 @@ class TransportType
      */
     private $transportTypes = [
         'mautic.transport.amazon'       => 'mautic.email.config.mailer_transport.amazon',
+        'mautic.transport.amazon_api'   => 'mautic.email.config.mailer_transport.amazon_api',
         'mautic.transport.elasticemail' => 'mautic.email.config.mailer_transport.elasticemail',
         'gmail'                         => 'mautic.email.config.mailer_transport.gmail',
         'mautic.transport.mandrill'     => 'mautic.email.config.mailer_transport.mandrill',
@@ -51,6 +52,7 @@ class TransportType
         'mautic.transport.sendgrid',
         'mautic.transport.elasticemail',
         'mautic.transport.amazon',
+        'mautic.transport.amazon_api',
         'mautic.transport.postmark',
         'gmail',
         // smtp is left out on purpose as the auth_mode will manage displaying this field
@@ -64,6 +66,7 @@ class TransportType
         'mautic.transport.sendgrid',
         'mautic.transport.elasticemail',
         'mautic.transport.amazon',
+        'mautic.transport.amazon_api',
         'mautic.transport.postmark',
         'gmail',
         // smtp is left out on purpose as the auth_mode will manage displaying this field
@@ -201,6 +204,14 @@ class TransportType
     public function getAmazonService()
     {
         return '"mautic.transport.amazon"';
+    }
+
+    /**
+     * @return string
+     */
+    public function getAmazonApiService()
+    {
+        return '"mautic.transport.amazon_api"';
     }
 
     /**
